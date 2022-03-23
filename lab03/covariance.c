0236f628ee444121c1e8bb06b956902727ce2df8
jzielins97 <jakub.stanislaw.zielinski@cern.ch>
2022-03-23 10:49:04 +0100

    Add exercise 3 (part 1)

--- lab03/covariance.c
+++ lab03/covariance.c
@@ -5,7 +5,9 @@
  * Date: 16.03.2022
  * 
  * Compilation command:
- *      gcc covariance.c -o cov_mpi -lm 
+ *      mpicc covariance.c -o cov_mpi -lm 
+ * Run command:
+ *      mpirun -np 20 ./cov_mpi
  * */
 
 #include <stdlib.h>
@@ -15,63 +17,105 @@
 #include <assert.h>
 #include <math.h>
 
+//include MPI library
+#include "mpi.h" 
+
 #include "mct_utils.h"
 
 #define NELEMENTS 134217728
 
 int main( int argc , char ** argv ) 
 {
-    int i, j;
-    
-    // allocate memory
-    double *var[5];
-    for(i=0; i<5; i++) cppmallocl(var[i],NELEMENTS,double);     
+  // Initialize MPI
+  int ip, np; // basic MPI indicators
+  char inmsg, outmsg='x';
+  MPI_Status Stat; // required variable for receive routines
+  MPI_Init( &argc, &argv ); // set up the parallel world
+  MPI_Comm_size( MPI_COMM_WORLD, &np ); // total number of processes
+  MPI_Comm_rank( MPI_COMM_WORLD, &ip ); // id of process st 0 <= ip < np
 
+  long int Nip, i0;  
+  int i, j;
+  double *var[5];
+  /* for(i=0; i<5; i++) cppmallocl(var[i],NELEMENTS,double);      */
+  FILE * fp;
+  char file_name[128];
+          
+
+  // distribute elements over processe
+  Nip = getNip(ip, np, NELEMENTS);
+  i0 = get_i0(ip, np, NELEMENTS);
+  size_t size = sizeof(double)*Nip;
+  printf("Process ip=%3d: Number of elements: %ld; Range: [%ld,%ld]; Size of array: %ld MB\n", ip, Nip, i0, i0+Nip-1, size/1024/1024);
+
+  // Allocate memory
+  for(i=0; i<5; i++) cppmallocl(var[i], Nip, double);
     
-    // Read binary data
-    b_t(); // start timing
-    FILE * fp;
-    char file_name[128];
-    for(i=0; i<5; i++)
-    {
-        sprintf(file_name, "/home2/archive/MST-2022/lab1/var%d.dat", i+1);
-        printf("# READING DATA: `%s`...\n", file_name);
-        
-        fp = fopen(file_name, "rb");
-        if(fp==NULL) { printf("# ERROR: Cannot open `%s`!\n", file_name); return EXIT_FAILURE; }
-        
-        size_t ftest = fread(var[i], sizeof(double), NELEMENTS, fp);
-        if(ftest!=NELEMENTS) { printf("# ERROR: Cannot read `%s`!\n", file_name); return EXIT_FAILURE; }
-        
-        fclose(fp);
-    }
-    double tio = e_t(); // stop timing
-    printf("# READ TIME: %f sec\n", tio);
+    
+  // Read binary data
+  b_t(); // start timing
+  // MPI_Rev(void* data, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm communicator, MPI_Status* status);
+  if(ip>0){
+    printf("\t# process %3d waiting", ip);
+    MPI_Recv(&inmsg, 1, MPI_CHAR,ip-1, 1, MPI_COMM_WORLD, &Stat ); // wait for signal from previous process
+  }
+
+  printf("\t# process %3 loading data", ip);
+  for(i=0; i<5; i++){
+      	
+    sprintf(file_name, "/home2/archive/MCT-2022/lab1/var%d.dat", i+1);
+    /* printf("# READING DATA: `%s`...\n", file_name); */
+      
+    fp = fopen(file_name, "rb");
+    if(fp==NULL) { printf("# ERROR: Cannot open `%s`!\n", file_name); return EXIT_FAILURE; }
+    
+    //  move file pointer to the beginning of the chunk for the given process
+    fseek( fp, i0, SEEK_SET);
+    // read chunk from the file
+    size_t ftest = fread(var[i], sizeof(double), Nip, fp);
+    printf("\tRead from file #%1d: %ld; Range: [%ld,%ld]; Size of array: %ld MB\n", i, ftest, i0, i0+Nip-1, sizeof(double)*ftest/1024/1024);
+    if(ftest!=Nip) { printf("# ERROR: Cannot read `%s`!\n", file_name); return EXIT_FAILURE; }
+
+    // close file
+    fclose(fp);
+	
+  }// end of reading files for loop
+
+  if(ip!=(np-1)){
+    //MPI_Send(void* data, int count, MPI_Datatype datatype, int destination, int tag, MPI_Comm communicator);
+    MPI_Send(&outmsg, 1, MPI_CHAR, ip+1, 1, MPI_COMM_WORLD); // send signal to process ip+1 that reading of my chunks is finished
+  }// end of if(ip!=(np-1))
+    
+  double tio = e_t(); // stop timing
+  printf("# READ TIME: %f sec\n", tio);
+    
+  long int idx[10] = {10,13421673,25501328,41606496,53677091,73818750,83214911,93952210,106032910,132875451};
+  for(i=0; i<5; i++) 
+    for(j=0; j<10; j++)
+      printf("var[%d][%10ld]=%16.8g\n", i,idx[j],var[i][idx[j]]);
+    
+  /* 
+  // generate additional random variables v_6, ..., v_10 and compute covariance matrix
+  // make computation only for elements cov(i,j) where i<=j (see printing statment)
+  double cov[10][10]; // storage for covaraince matrix
+  b_t(); // start timing
+    
+  // TODO: add your code here!
+    
+  double tcmp = e_t(); // stop timing
+  printf("# COMPUTATION TIME: %f sec\n", tcmp);
+  
+  // print results
+  for(i=0; i<10; i++) for(j=0; j<=i; j++)
+  {
+  printf("cov(%2d,%2d)=%16.8g\n", i+1, j+1, cov[i][j]);
+  }
+  */
 
 
-    long int idx[10] = {10,13421673,25501328,41606496,53677091,73818750,83214911,93952210,106032910,132875451};
-    for(i=0; i<5; i++) 
-      for(j=0; j<10; j++)
-	printf("var[%d][%10ld]=%16.8g\n", i,idx[j],var[i][idx[j]]);
+  // done with MPI
+  MPI_Finalize();
     
-    /* 
-    // generate additional random variables v_6, ..., v_10 and compute covariance matrix
-    // make computation only for elements cov(i,j) where i<=j (see printing statment)
-    double cov[10][10]; // storage for covaraince matrix
-    b_t(); // start timing
-    
-    // TODO: add your code here!
-    
-    double tcmp = e_t(); // stop timing
-    printf("# COMPUTATION TIME: %f sec\n", tcmp);
-    
-    // print results
-    for(i=0; i<10; i++) for(j=0; j<=i; j++)
-    {
-        printf("cov(%2d,%2d)=%16.8g\n", i+1, j+1, cov[i][j]);
-    }
-    */
-    
-    return( EXIT_SUCCESS ) ;
+  return( EXIT_SUCCESS ) ;
 }
 
