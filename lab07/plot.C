42271119303b6ca4336c1a15f006076a5342f1a0
jzielins97 <jakub.stanislaw.zielinski@cern.ch>
2022-05-02 12:39:50 +0200

    Add plotting macro for task 6

--- lab07/plot.C
+++ lab07/plot.C
@@ -14,15 +14,22 @@ void plot(double omega = 0.001){
     std::cout<<"Error while reading dimension of the vectors"<<std::endl;
     exit(1);
   }
+  std::cout<<"Vectors are "<<N<<"-dimensional"<<std::endl;
   double valY;
   
   TMultiGraph* mg = new TMultiGraph();
   int n = 0;
+  int nCorrect = 0;
   std::cout<<"Compare Eigenvalues"<<std::endl;
   std::cout<<" Numerical  | Analytical | difference"<<std::endl;
   std::cout<<"_____________________________________"<<std::endl;
   while(fValues>>eigenvalue){
+    double En = omega *(n + 0.5);
+    double error = TMath::Abs(En - eigenvalue);
+    if(error<0.0001) nCorrect++;
+    
     if(n<5){
+      printf("%12.6lf|%12.6lf|%12.6lf\n",eigenvalue, En, error);
       TGraph* gr = new TGraph();
       for(int i=0; i<N; i++){
 	fVectors>>valY;
@@ -32,13 +39,13 @@ void plot(double omega = 0.001){
       gr->SetTitle(Form("#lambda=%lf;x [a.u.];#Psi(x)",eigenvalue));
       mg->Add(gr);
     }
-    n++;
-    double En = omega *(n + 0.5);
-    printf("%lf5.6|%lf5.6|%lf5.6",eigenvalue, En, TMath::Abs(En - eigenvalue));
+    
+    n++;  
   }
+  std::cout<<"Calculated "<<nCorrect<<" eigenvalues that were closer than 0.001"<<std::endl;
 
   TCanvas* c = new TCanvas("c", "Canva", 10, 10, w, h);
-  c->DrawFrame(0,0, 400, 10);
+  c->DrawFrame(0,0, 400, 1);
   mg->Draw("l");
   mg->GetHistogram()->SetTitle("First five eigenvectors;x [a.u]; #Psi(x)");
   c->SaveAs("eigenvectors.png");
